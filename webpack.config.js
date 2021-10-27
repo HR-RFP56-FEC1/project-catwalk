226fbbe075875742100e58c41d71530c3b6c5cb0
Brett Tokairin <bestbrett@aol.com>
2021-10-27 08:04:20 -1000

    added webpack and initial folder structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+};
