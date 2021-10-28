ca54a4be3a347ff057638a3586fe169e4a9cb6eb
Unknown <bestbrett@aol.com>
2021-10-28 12:58:24 -1000

    started question components

---
+++ client/src/components/questions/Qsearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Qsearch = () => (
+  <div id='Qsearch'>
+    <div id='qsearchtextinput'>PLACEHOLDER TEXT</div>
+    <div id='qbutton'>MAGNIFYING GLASS ICON</div>
+  </div>
+);
+
+export default Qsearch
