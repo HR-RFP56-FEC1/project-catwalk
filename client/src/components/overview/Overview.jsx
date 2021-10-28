dde75dbaf9e1cd1322aacf4dfe172a8078e9662a
Michael Fornwalt <mdfornwalt@gmail.com>
2021-10-28 14:52:16 -0700

    overview skeletonized with divs. overview-styles.css added, linked in index.html, and working.

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 const Overview = () => (
   <div id='overview'>
-    <div id='logo'></div>
+    <div id='logo'>TESTING LOGO</div>
     <div id='announcement'></div>
     <div id='body'>
-      <div id='left'></div>
-      <div id='right'></div>
+      <div id='left'>
+        <div id='thumbs'></div>
+        <div id='product'>
+          <div id='photo'></div>
+          <div id='description'></div>
+        </div>
+      </div>
+      <div id='right'>
+        <div id='details'></div>
+        <div id='styles'></div>
+        <div id='buttons'></div>
+        <div id='bullet points'></div>
+      </div>
     </div>
   </div>
 )
