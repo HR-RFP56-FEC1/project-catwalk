eb1c1b1c8a738dea78d4e8cfd1973980deed79d3
Michael Fornwalt <mdfornwalt@gmail.com>
2021-10-28 20:38:32 -0700

    div size and space set up by css. working on layout

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -3,16 +3,16 @@ import { useState } from 'react'
 
 
 const Thumbs = () => {
-  const [thumbs, setThumbs] = useState([1, 2, 3, 4, 5, 6, 7])
+  const [thumbs, setThumbs] = useState([1, 2, 3, 4])
   console.log('THUMBS: ', thumbs)
   return (
-    // <div id='thumbs'>{thumbs.map(thumb => <div>thumb</div>)}</div>
+    <div id='thumbs'>{thumbs.map((thumb, i) => <Thumb key={i} />)}</div>
   )
 }
 
-// const Thumb = () => (
-//   <div className='thumb'>Thumb</div>
-// )
+const Thumb = (props) => (
+  <div className='thumb'>{props.i}T</div>
+)
 
 
 const Overview = () => (
@@ -20,13 +20,25 @@ const Overview = () => (
     <div id='logo'>TESTING LOGO</div>
     <div id='announcement'><i>SITE-WIDE ANNOUNCEMENT MESSAGE!</i> - SALE / DISCOUNT <b>OFFER</b> - <u>NEW PRODUCT HIGHLIGHT</u></div>
     <div id='body'>
+
       <div id='left'>
-        <Thumbs />
-        <div id='product'>
-          <div id='photo'></div>
+        <div id='main-image'>
+          <div id='thumbs-left-arrow-container'>
+            <Thumbs />
+            <div id='arrow-column-left'>
+              <div id='arrow-left'></div>
+            </div>
+          </div>
+          <div id='arrow-column-right'>
+            <div id='arrow-right'></div>
+          </div>
+        </div>
+        <div id='description-container'>
+          <div id='description-spacer'></div>
           <div id='description'></div>
         </div>
       </div>
+
       <div id='right'>
         <div id='details'></div>
         <div id='styles'></div>
