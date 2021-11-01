f4fe33f8535c891376fd3df29ff1167830ff0958
Unknown <bestbrett@aol.com>
2021-11-01 10:32:45 -1000

    added api call to constructor, needs testing

--- client/src/components/questions/QnA.jsx
+++ client/src/components/questions/QnA.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import Qsearch from './Qsearch.jsx';
 import QuestionList from './QuestionList.jsx';
-
+import axios from 'axios';
 import questions from '../../../sample/questions.js';
+
+const getQuestions = function(id) {
+  axios.get({
+    url: '/api/qa/questions' + '?product_id=' + id + '&count=100',
+  }).then((data) => {
+    return <QuestionList questions={data.results}/>
+  });
+};
 
 
 const QnA = (props) => (
   <div id="qna" className='qna'>
     <div id='qtitlebar'>QUESTIONS AND ANSWERS</div>
     <div ><Qsearch/></div>
-    <div id='qList'><QuestionList questions={questions}/></div>
+    <div id='qList'>{getQuestions(props.id)}</div>
   </div>
 );
+
+// const QnA = (props) => (
+//   <div id="qna" className='qna'>
+//     <div id='qtitlebar'>QUESTIONS AND ANSWERS</div>
+//     <div ><Qsearch/></div>
+//     <div id='qList'><QuestionList questions={questions}/></div>
+//   </div>
+// );
 
 export default QnA;
