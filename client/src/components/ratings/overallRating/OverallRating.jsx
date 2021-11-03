34d1e8a392975d85c164299756a33b1128a76d1c
NABAO <xyinaxy@gmail.com>
2021-11-02 18:18:13 -0700

    refactor overall rating component to render stuff only when data is not null (render after useEffect)

--- client/src/components/ratings/overallRating/OverallRating.jsx
+++ client/src/components/ratings/overallRating/OverallRating.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import reviewmeta from '../../../../sample/reviewmeta.js';
@@ -9,55 +9,58 @@ import ProductBreakdown from './productBreakdown/ProductBreakdown.jsx';
 
 let id = '40346';
 
-const getRatings = function(id) {
-  var urlString = `/api/reviews/meta?product_id=${id}`;
+const getRatings = function (id) {
+  var urlString = `/api/reviews/meta?product_id=${id}&count=100`;
   return axios({
     method: 'get',
     url: urlString,
-    headers: {},
     responseType: 'json'
   });
 }
 
 var OverallRating = (props) => {
-  const [overallRating, setRating] = useState(reviewmeta);
+  const [overallRating, setRating] = useState(null);
 
   useEffect(() => {
     getRatings(id)
       .then(res => {
-        console.log('ratings data:', res.data);
+        // console.log('ratings data:', res.data);
         setRating(res.data);
       })
   }, [])
 
-  console.log('current state:', overallRating.ratings);
-  console.log('sample data:', reviewmeta);
+  if (overallRating !== null) {
+    let allRating = overallRating.ratings;
+    // console.log(allRating);
+    let sumRating = 0;
+    let countRating = 0;
+    for (const key in allRating) {
+      let numRating = parseInt(key);
+      // console.log(numRating);
+      sumRating += numRating * allRating[key];
+      countRating += parseInt(allRating[key]);
+    }
+    const avgRating = sumRating / countRating;
+    const recommend = overallRating.recommended;
+    const recommendCount = parseInt(recommend.true) || 0;
+    const notRecommendCount = parseInt(recommend.false) || 0;
+    const recommendPerc = recommendCount / (recommendCount + notRecommendCount);
 
-  let allRating = overallRating.ratings;
-  // console.log(allRating);
-  let sumRating = 0;
-  let countRating = 0;
-  for (const key in allRating) {
-    let numRating = parseInt(key);
-    // console.log(numRating);
-    sumRating += numRating * allRating[key];
-    countRating += parseInt(allRating[key]);
+    return (
+      <div className="overall-rating">
+        <RatingSummary avgRating={avgRating} />
+        <RatingDist ratingDist={overallRating.ratings} recommendPerc={recommendPerc}
+          totRatings={countRating} />
+        <ProductBreakdown breakdown={overallRating.characteristics} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        Loading reviews...
+      </div>
+    )
   }
-  const avgRating = sumRating / countRating;
-  const recommend = overallRating.recommended;
-  const recommendCount = parseInt(recommend.true) || 0;
-  const notRecommendCount = parseInt(recommend.false) || 0;
-  const recommendPerc = recommendCount/(recommendCount + notRecommendCount);
-  // console.log(recommendCount/(recommendCount + notRecommendCount));
-
-  return(
-    <div className="overall-rating">
-      <RatingSummary avgRating={avgRating}/>
-      <RatingDist ratingDist={overallRating.ratings} recommendPerc={recommendPerc}
-      totRatings={countRating}/>
-      <ProductBreakdown breakdown={overallRating.characteristics}/>
-    </div>
-  )
 };
 
 export default OverallRating;
